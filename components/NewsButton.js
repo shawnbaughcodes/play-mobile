24faca2c058f368a8d0a9abbd00131c59cda6d1b
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-12-20 01:57:30 -0600

    Completed styling on News page.

---
+++ components/NewsButton.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { View, Text, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+class NewsButton extends Component {
+    render() {
+        return (
+            <View style={styles.containerStyle}>
+                <View style={styles.titleBGStyle}>
+                    <Text style={styles.textStyle}>{this.props.title}</Text>
+                </View>
+                <View style={styles.imageStyle}>
+                    {this.props.image}
+                </View>
+            </View>
+        );
+    }
+}
+const styles = {
+    containerStyle: {
+        flex: 3,
+        flexDirection: 'row',
+        height: 140,
+        marginLeft: 10,
+        marginRight: 10,
+        backgroundColor: '#DADFF7',
+        borderRadius: 10,
+        marginTop: 15
+    },
+    textStyle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#232C33'
+    },
+    titleBGStyle: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        borderTopRightRadius: 25,
+        borderBottomRightRadius: 25,
+        backgroundColor: '#A0C1D1',
+        width: 40,
+        borderColor: '#B5B2C2',
+        borderWidth: 0.7
+    },
+    imageStyle: {
+        flex: 2,
+        flexDirection: 'column',
+    }
+};
+export default NewsButton;
--- screens/NewsScreen.js
+++ screens/NewsScreen.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Dimensions } from 'react-native';
+import { Container, Header, Body, Title } from 'native-base';
 
+import NewsButton from '../components/NewsButton';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 class NewsScreen extends Component {
-    
+
     render() {
         return (
-            <View style={styles.newsStyle}>
-                <Text style={{ color: 'white' }}>News Screen</Text>
-                <Text style={{ color: 'white' }}>News Screen</Text>
-                <Text style={{ color: 'white' }}>News Screen</Text>
-                <Text style={{ color: 'white' }}>News Screen</Text>
-                <Text style={{ color: 'white' }}>News Screen</Text>
-                <Text style={{ color: 'white' }}>News Screen</Text>
-                <Text style={{ color: 'white' }}>News Screen</Text>
-                <Text style={{ color: 'white' }}>News Screen</Text>
-                <Text style={{ color: 'white' }}>News Screen</Text>
-                <Text style={{ color: 'white' }}>News Screen</Text>
-            </View>
+            <Container style={styles.newsStyle}>
+                <Header style={styles.headerStyle}>
+                    <Body>
+                        <Title style={{ color: 'white' }}>News</Title>
+                    </Body>
+                </Header>
+                <ScrollView style={{ marginTop: 10 }}>
+                    <NewsButton
+                        title='Football'
+                    />
+                    <NewsButton
+                        title='Basketball'
+                    />
+                    <NewsButton
+                        title='Baseball'
+                    />
+                    <NewsButton
+                        title='Soccer'
+                    />
+                    <NewsButton
+                        title='Tennis'
+                    />
+                    <NewsButton
+                        title='Volleyball'
+                    />
+                    <NewsButton
+                        title='Golf'
+                    />
+                </ScrollView>
+            </Container>
         );
     }
 }
@@ -24,6 +46,10 @@ const styles = {
     newsStyle: {
         flex: 1,
         backgroundColor: '#333'
+    },
+    headerStyle: {
+        backgroundColor: 'black',
+        opacity: 0.3
     }
 };
 export default NewsScreen;
